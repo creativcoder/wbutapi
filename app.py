58a4f4e2750c1ec406a08c6ab1a7e14d41b87e85
creativcoder <rsconceptx@gmail.com>
2015-08-16 01:11:58 +0530

    Added required header fields

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import requests
 from lxml import html
 from datetime import datetime
+
+headers = {
+        'Origin': 'http://wbutech.net',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Referer': 'http://wbutech.net/result_even.php',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+    }
